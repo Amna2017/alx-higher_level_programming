bc3212505697d2592d3869341b04726cf4f389e9
Amna2017 <aiaashour2017@gmai.com>
2023-06-07 20:00:21 -0700

    my commit

---
+++ 0x01-python-if_else_loops_functions/6-print_comp3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for gg in  range(0, 90):
+        if gg != 89:
+                     print("%d, "(gg.zfill(2))
+                                 if gg == 89:
+                                          print("%d \n"(gg.zfill(2)
+                                              )
