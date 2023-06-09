ad932a61ff8f6784ac23fcf6eca78397b52ec351
Amna2017 <aiaashour2017@gmai.com>
2023-06-09 03:18:56 -0700

    my commit

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -3,5 +3,5 @@ for n in range(100):
     if n >= 0 and n < 99:
         n = str(n)
         n = n.zfill(2)
-        print("{:s}, ".format(n),end="")
+        print("{:s}, ".format(n), end="")
 print(99)
