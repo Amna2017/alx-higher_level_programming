e9749f5d8cae621c5baa194a2b37b76bb9bfd3e7
Amna2017 <aiaashour2017@gmai.com>
2023-06-10 08:38:10 -0700

    my commit

--- 0x01-python-if_else_loops_functions/6-print_comp3.py
+++ 0x01-python-if_else_loops_functions/6-print_comp3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for gg in  range(0, 90):
-        if gg != 89:
-                     print("%d, "(gg.zfill(2))
-                                 if gg == 89:
-                                          print("%d \n"(gg.zfill(2)
-                                              )
+for n in range(100):
+    if n == 0 or n > 0 and n < 99:
+        m = str(n)
+        n = m.zfill(2)
+        print("{:s}, ".format(n), end="")
+print(99)
