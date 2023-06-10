33c09dfb1fd78d43673a7e5a28bc5016d3a2f770
Amna2017 <aiaashour2017@gmai.com>
2023-06-10 10:33:39 -0700

    yarb 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for amna in range(99):
-    if amna == 0 or amna > 0 and amna < 98:
+    if (amna == 1 or amna > 1) and amna < 98:
         amna = str(amna).zfill(2)
         print("{:s}, ".format(amna), end="")
 print("98")
--- 0x01-python-if_else_loops_functions/6-print_comp3.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-for amna in range(99):
-    if amna == 0 or amna > 0 and amna < 98:
-        amna = str(amna).zfill(2)
-        print("{:s}, ".format(amna), end="")
-print("98")
