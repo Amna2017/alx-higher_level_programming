2ded249a2c2f05fd04ba7fbdd01a094cdc469147
Amna2017 <aiaashour2017@gmai.com>
2023-06-10 08:46:22 -0700

    my commit

--- 0x01-python-if_else_loops_functions/6-print_comp3.py
+++ 0x01-python-if_else_loops_functions/6-print_comp3.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 for n in range(100):
-    if n == 0 or n > 0 and n < 99:
-        m = str(n)
-        n = m.zfill(2)
-        print("{:s}, ".format(n), end="")
-print(99)
+    if n == 0 or n > 0 and n < 98:
+        n = str(n).zfill(2)
+        print("{:s}, ".format(str(n).zfill(2)), end="")
+print(98)
