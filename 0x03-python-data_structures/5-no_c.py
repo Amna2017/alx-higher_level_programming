028d49ef33c8ed5616657c3e69881ba6c327065d
Amna2017 <aiaashour2017@gmai.com>
2023-06-11 16:14:37 -0700

    0x03 project

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    list_ = []
+    num = len(my_string)
+    for ff in range(num):
+        if ord(my_string[ff]) == 99 or ord(my_string[ff]) == 67:
+            pass
+        else:
+            list_.append(my_string[ff])
+            yy = ''.join(list_)
+    return yy
