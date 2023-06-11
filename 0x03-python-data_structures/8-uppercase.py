f34b8dbe21b10c334431fb2536f72810556c2c50
Amna2017 <aiaashour2017@gmai.com>
2023-06-11 14:40:27 -0700

    0x03 project

--- 0x03-python-data_structures/8-uppercase.py
+++ 0x03-python-data_structures/8-uppercase.py
@@ -3,7 +3,7 @@ def uppercase(str):
     leng = len(str)
     for xx in range(leng):
         yy = str[xx]
-        if ord(yy) >64 and ord(yy) < 90:
+        if ord(yy) > 64 and ord(yy) < 90:
             print(yy, end="")
         else:
             print("{:s}".format(chr(ord(yy) - 32)), end="")
