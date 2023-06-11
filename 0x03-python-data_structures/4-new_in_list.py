a9184cbbdbb62afdf7fd5db2e539c26301670d1a
Amna2017 <aiaashour2017@gmai.com>
2023-06-11 16:55:54 -0700

    0x03 project

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,7 +2,7 @@
 def new_in_list(my_list, idx, element):
     new = my_list.copy()
     if idx < 0 or idx >= len(new):
-            return new
+        return new
     else:
         del new[idx]
         new.insert(idx, element)
