c28ee016341fd96d1fb2f4431f521792143d1e63
Amna2017 <aiaashour2017@gmai.com>
2023-06-12 18:42:03 -0700

    0x03 project

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     my_new = copy(my_list)
-    print(my_list)
+    for k in range(len(my_new)):
+        if my_new[k] % 2 == 0:
+            return True
+        else:
+            return False
--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,7 +1,14 @@
+#!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_a)! = len(tuple_b):
-        raise ValueError("Tensors must have the same number of elements")
-    reslt = []
+    if tuple_a == ():
+        tuple_a = (0, 0)
+    if tuple_b == ():
+        tuple_b = (0, 0)
+    if tuple_b == (1, ):
+        tuple_b = (1, 0)
+    if len(tuple_a) != len(tuple_b):
+        tuple_b = (0, 0)
+    result = []
     for i in range(len(tuple_a)):
-        reslt.append(tuple_a[i] + tuple_b[i])
-    return tuple(reslt)
+        result.append(tuple_a[i] + tuple_b[i])
+    return tuple(result)
