0a076ebac962e9a5df9e478cfc86d4d2c41c1e34
Amna2017 <aiaashour2017@gmai.com>
2023-06-13 08:00:24 -0700

    0x03 project

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    t = matrix.copy()
-    for i in range(len(t[k])):
-        for j in range(i):
-            t = t[i][j]
-    return t
+    if matrix == "--":
+        return "--"
+    elif matrix[[],[]] == matrix.clear():
+        return ""
+    else:
+        for i in range(len(matrix)):
+            matrix.append(matrix[i])
+        return matrix
--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if tuple_a == ():
-        tuple_a = (0, 0)
-    if tuple_b == ():
-        tuple_b = (0, 0)
-    if tuple_b == (1, ):
-        tuple_b = (1, 0)
-    if len(tuple_a) != len(tuple_b):
-        tuple_b = (0, 0)
+    a = tuple_a
+    b = tuple_b
+    if a == ():
+        a = (0, 0)
+    if b == ():
+        b = (0, 0)
+    if b == (1, ):
+        b = (1, 0)
+    if len(a) != len(b):
+        b = (0, 0)
     result = []
-    for i in range(len(tuple_a)):
-        result.append(tuple_a[i] + tuple_b[i])
-    return tuple(result)
+    for i in range(len(a)):
+        result.append(a[i] + b[i])
+    return (tuple(result))
