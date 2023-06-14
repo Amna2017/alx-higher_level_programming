2882592eb6cbae7ee92afd395c85fc38d766a5a5
Amna2017 <aiaashour2017@gmai.com>
2023-06-14 00:23:22 -0700

    0x02

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,5 +1,17 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import argparse
     import sys
-    print(len(argv))
+    from collections import deque
+    x = list(sys.argv)
+    z = deque(x)
+    y = z.popleft()
+    g = len(y)
+    if g == 0:
+        print("0 : argument.")
+    elif g == 1:
+        print("1  argument:")
+        print("{:d} : {:s}".format(g, x[g]))
+    else:
+        print(g, " arguments:")
+        for ff in range(g):
+            print("{:d} : {:s}".format((ff), y[ff]))
