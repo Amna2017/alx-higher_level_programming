feee0b3031b50940a3cf86afecc12fa8200299a2
Amna2017 <aiaashour2017@gmai.com>
2023-06-15 08:38:37 -0700

    my 0x04 python

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    unique_numbers=list(set(my_list))
+    unique_numbers = list(set(my_list))
     xd = 0
     for i in range(len(unique_numbers)):
         xd = xd + unique_numbers[i]
