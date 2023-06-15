b61fce6b11c702635fcec2b3d5174a18e9f6d823
Amna2017 <aiaashour2017@gmai.com>
2023-06-15 08:36:19 -0700

    my 0x04 python

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    unique_numbers=list(set(my_list))
+    xd = 0
+    for i in range(len(unique_numbers)):
+        xd = xd + unique_numbers[i]
+    return xd
