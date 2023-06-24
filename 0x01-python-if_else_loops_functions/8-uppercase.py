7fa66c789d4a1300b08ecf35ec779f048742ef6b
Amna2017 <aiaashour2017@gmai.com>
2023-06-24 01:07:00 -0700

    0x01-py commit

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
 def uppercase(str):
-    str = str.upper()
-    return str
+    # Loop through each character in the string
+    for c in str:
+        # If the character is a lowercase letter, convert it to uppercase
+        if ord(c) >= 97 and ord(c) <= 122:
+            c = chr(ord(c) - 32)
+        # Print the character without a new line
+        print("{}".format(c), end='')
+    # Print a new line at the end
+    print()
