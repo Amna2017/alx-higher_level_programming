8d6df2332772cae82bdf2edc63e6e0c1dea76195
Amna2017 <aiaashour2017@gmai.com>
2023-06-26 12:46:03 -0700

    ,,nnn

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-    while(1):
-        try:
-            print("{:d}".format(value))
-            return True
-        except SyntaxError:
-            return False
-        except ValueError:
-            return False
+    try:
+        print("{:d}".format(value))
+        return True
+    except SyntaxError:
+        return False
+    except ValueError:
+        return False
