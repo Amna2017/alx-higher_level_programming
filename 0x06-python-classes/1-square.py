5ece3a11f72781d1c0342d0ebeeb1df95a00eba7
Amna2017 <aiaashour2017@gmai.com>
2023-07-03 17:41:26 -0700

    nmm

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+""" Write an empty class Square that defines a squar """
+
+
 class Square:
+    """ this is init of class """
     def __init__(self, size):
+        """this is 1st function """
         self.__size = size
