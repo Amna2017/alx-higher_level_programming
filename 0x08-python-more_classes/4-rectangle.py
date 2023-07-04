9295b6ec29e103bd4dc0d85fda8c0f811c2677ff
Amna2017 <aiaashour2017@gmai.com>
2023-07-03 19:58:01 -0700

    0x08-python-more_classes

---
+++ 0x08-python-more_classes/4-rectangle.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+""" my new class """
+
+
+class Rectangle:
+    """ my new class """
+    def __init__(self, width=0, height=0):
+        """ my new class """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """ my new class """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ my new class """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """ my new class """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ my new class """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """ my new class """
+        return self.width * self.height
+
+    def perimeter(self):
+        """ my new class """
+        return 2 * (self.width + self.height)
+
+    def __str__(self):
+        """ my new class """
+        if self.width == 0 or self.height == 0:
+            return ""
+        rectangle_str = ""
+        for _ in range(self.height):
+            rectangle_str += "#" * self.width + "\n"
+        return rectangle_str.strip()
+
+    def __repr__(self):
+        """ my new class """
+        return f"Rectangle({self.width}, \
+                {self.height})"
