b61f99090b75d8ddff86ca2240bebdc1a39eb858
Amna2017 <aiaashour2017@gmai.com>
2023-07-03 17:46:26 -0700

    nmm

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
+"""Write an empty class Square that defines a square"""
+
+
 class Square:
+    """my init of class   """
     def __init__(self, size=0):
+        """this is init function """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
