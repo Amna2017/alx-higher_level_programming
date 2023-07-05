53436a156c7809662f4caa1239a14634ca3d5f3e
­Amna2017 <asiaashour2017@gmail.com>
2023-07-04 19:57:17 -0700

    n

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+def add_integer(a, b=98):
+    """ hhh """
+    # Check if a and b are integers or floats
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer or b must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("a must be an integer or b must be an integer")
+
+    # Cast a and b to integers if they are floats
+    if isinstance(a, float):
+        a = int(a)
+    if isinstance(b, float):
+        b = int(b)
+
+    # Return the addition of a and b
+    return a + b
