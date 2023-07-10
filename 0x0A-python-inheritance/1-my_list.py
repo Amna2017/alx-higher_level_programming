326160984a566b30a925a2d51937a57456d41b31
­Amna2017 <asiaashour2017@gmail.com>
2023-07-10 11:25:12 -0700

    2 nd commit

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" my class """
+
+
+class MyList(object):
+    def print_sorted(self):
+        """  my sort func """
+        return dir(self).sort()
---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""  my check class """
+
+
+def is_same_class(obj, a_class):
+    """ my check func"""
+    if type(obj) is a_class:
+        return True
+    else:
+        return False
