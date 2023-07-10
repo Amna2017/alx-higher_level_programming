c5adaf0595737abd0dcfed392d45ea57f3bfb0fa
­Amna2017 <asiaashour2017@gmail.com>
2023-07-10 12:16:38 -0700

    0x0A commit

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""my class """
+
+
+def inherits_from(obj, a_class):
+    """ my func"""
+    if obj or isinstance(obj, a_class):
+        return True
+    else:
+        return False
