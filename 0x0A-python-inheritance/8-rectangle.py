7e68c8ac0cbc61851abe21969e4370e3b397756e
­Amna2017 <asiaashour2017@gmail.com>
2023-07-10 15:55:06 -0700

    mm 0x0A

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""" my class """
+
+
+class BaseGeometry:
+    """ my class """
+    def area(self):
+        """ my class func """
+        raise NotImplementedError("area() method not implemented")
+
+    def perimeter(self):
+        """ my class func """
+        raise NotImplementedError("perimeter() method not implemented")
+
+
+class Rectangle(BaseGeometry):
+    """ my class """
+    def __init__(self, width, height):
+        """ my class func """
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+    def integer_validator(self, name, value):
+        """ my class func """
+        if not isinstance(value, int) or value <= 0:
+            raise ValueError(f"{name} must be a positive integer")
+
+    def area(self):
+        """ my class func """
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """ my class func """
+        return 2 * (self.__width + self.__height)
