240cfb8c27be1e2ebf4ab3d53d4bbcfc6a20cec2
­Amna2017 <asiaashour2017@gmail.com>
2023-07-10 12:01:03 -0700

    0x0A commit

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -4,9 +4,7 @@
 
 def is_kind_of_class(obj, a_class):
     """ my check if enheritance or no """
-    if type(obj) is a_class:
-        return True
-    elif isinstance(type(obj), a_class):
+    if type(obj) is a_class or isinstance(type(obj), a_class):
         return True
     else:
         return False
