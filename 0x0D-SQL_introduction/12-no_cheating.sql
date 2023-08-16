467d1dd005705ee556e3939d1d7ec4bb4f12bca8
Amna <amnataha2011@gmail.com>
2023-08-16 14:29:55 -0700

    mmff

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,4 +1,4 @@
---ubdate the score of Bob to 10 in the second_table using name only
+-- ubdate the score of Bob to 10 in the second_table using name only.
 UPDATE 'second_table'
 SET 'score' = 10
 WHERE 'name' = 'Bob';
