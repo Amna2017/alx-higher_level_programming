eeec41712325422da18d6ecde94c95031e18e069
­Amna2017 <asiaashour2017@gmai.com>
2023-09-13 15:17:08 -0700

    mmm

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -6,7 +6,7 @@ function add (a, b) {
 const num1 = parseInt(process.argv[2]);
 const num2 = parseInt(process.argv[3]);
 if (isNaN(num1) || isNaN(num2)) {
-  console.log('Invalid input. Please provide two integers.');
+  console.log('NaN');
 } else {
-  console.log('The sum is:', add(num1, num2));
+  console.log(add(num1, num2));
 }
