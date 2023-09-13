766afb258c3b0816a90db6811f0291139765464b
­Amna2017 <asiaashour2017@gmai.com>
2023-09-13 15:32:10 -0700

    mmm

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -3,11 +3,11 @@
 function factorial (n) {
   if (isNaN(n)) {
     return 1;
- } else if (n === 0) {
-	return 1;
-} else {
+  } else if (n === 0) {
+    return 1;
+  } else {
     return n * factorial(n - 1);
-	}
+  }
 }
 const num = parseInt(process.argv[2]);
 console.log(factorial(num));
