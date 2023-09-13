ca9dad20f168e88248f5ac6dead053b352b51ed5
­Amna2017 <asiaashour2017@gmai.com>
2023-09-13 15:56:42 -0700

    mmm

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+
+function findSecondBiggestInt (...args) {
+  if (args.length < 2) {
+    return 0;
+  }
+  const sortedArgs = args.map(Number).sort((a, b) => b - a);
+  return sortedArgs[1];
+}
+const secondBiggestInt = findSecondBiggestInt(...process.argv.slice(2));
+console.log(secondBiggestInt);
