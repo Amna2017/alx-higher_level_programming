77aee6cfdde728b7190196d05f86d02e7d4930f2
Amna2011 <amnataha2011@gmail.com>
2023-09-17 18:31:28 -0700

    mmm

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 
 import MySQLdb
-import sys 
+import sys
+
+
 def list_states(username, password, database):
-    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
+    """my func to db"""
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=username, passwd=password, db=database)
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states ORDER BY id ASC")
     rows = cursor.fetchall()
@@ -11,8 +15,10 @@ def list_states(username, password, database):
         print(row)
     cursor.close()
     db.close()
+
+
 if __name__ == '__main__':
     username = sys.argv[1]
     password = sys.argv[2]
-    database = sys.argv[3] 
+    database = sys.argv[3]
     list_states(username, password, database)
