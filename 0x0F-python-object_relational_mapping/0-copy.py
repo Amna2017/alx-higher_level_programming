68715cb474acff2225513352beaae63a2d631c2b
Amna2011 <amnataha2011@gmail.com>
2023-09-17 18:20:25 -0700

    mmm

---
+++ 0x0F-python-object_relational_mapping/0-copy.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+import MySQLdb
+
+import sys
+
+if __name__ == '__main__':
+    # Get MySQL credentials
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    # Connect to MySQL server
+    db = MySQLdb.connect(host="localhost", port=3306, user=username,
+              passwd=password, db=database)
+
+# Create a cursor object to execute SQL queries
+            cursor = db.cursor()
+
+# Execute the SQL query to fetch all states
+            cursor.execute("SELECT * FROM states ORDER BY id ASC")
+            # Fetch all rows from the result set
+            rows = cursor.fetchall()
+            # Display the results
+
+            for row in rows:                                         print(row)
+           # Close the cursor and database connection
+cursor.close()
+db.close()
--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,28 +1,18 @@
 #!/usr/bin/python3
+
 import MySQLdb
-
-import sys
-
+import sys 
+def list_states(username, password, database):
+    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    db.close()
 if __name__ == '__main__':
-    # Get MySQL credentials
     username = sys.argv[1]
     password = sys.argv[2]
-    database = sys.argv[3]
-
-    # Connect to MySQL server
-    db = MySQLdb.connect(host="localhost", port=3306, user=username,
-              passwd=password, db=database)
-
-# Create a cursor object to execute SQL queries
-            cursor = db.cursor()
-
-# Execute the SQL query to fetch all states
-            cursor.execute("SELECT * FROM states ORDER BY id ASC")
-            # Fetch all rows from the result set
-            rows = cursor.fetchall()
-            # Display the results
-
-            for row in rows:                                         print(row)
-           # Close the cursor and database connection
-cursor.close()
-db.close()
+    database = sys.argv[3] 
+    list_states(username, password, database)
